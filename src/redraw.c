ae08a3774cfa6ff8414bfe92940e107a79444500
Alia Salem <Alia@Alias-MacBook.local>
2022-05-24 20:01:51 +0400

    added the counter on the window

--- src/redraw.c
+++ src/redraw.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redraw.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aalsuwai <aalsuwai@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Alia <Alia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 18:44:04 by anasr             #+#    #+#             */
-/*   Updated: 2022/05/24 15:18:59 by aalsuwai         ###   ########.fr       */
+/*   Updated: 2022/05/24 20:00:18 by Alia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,14 @@ void	redraw_window(t_main *s)
 	if (s->minimap_on)
 		draw_minimap(s);
 
+		
+
 	/* putting the image to the window */
 	mlx_put_image_to_window(s->mlx, s->mlx_window, s->mlx_image, 0, 0);
+	if (s->p->full_sprite_count)
+	{
+		mlx_string_put(s->mlx, s->mlx_window, 440, 30, 0x567d46, "Leaf: ");
+		mlx_string_put(s->mlx, s->mlx_window, 490, 30, 0x567d46, ft_itoa(s->p->full_sprite_count));
+	}
 }
 
