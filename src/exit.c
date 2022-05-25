b0545842d76e53e062abf6f7f5caeacd2bf21c17
Alia Alsuwaidi <aalsuwai@lab3r2s4.42abudhabi.ae>
2022-05-25 19:03:16 +0400

    freed everyting in the exit function

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: aalsuwai <aalsuwai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 16:45:15 by anasr             #+#    #+#             */
-/*   Updated: 2022/05/25 18:34:12 by aalsuwai         ###   ########.fr       */
+/*   Updated: 2022/05/25 18:54:11 by aalsuwai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,31 @@ void	free_double_char(char **array)
 /* function to exit the program */
 int	close_x(t_main *s)
 {
+	int	i;
+	
+	//destroy textures
+	mlx_destroy_image(s->mlx, s->start_screen.image);
+	mlx_destroy_image(s->mlx, s->end_screen.image);
 	mlx_destroy_image(s->mlx, s->texture[0].image);
 	mlx_destroy_image(s->mlx, s->texture[1].image);
 	mlx_destroy_image(s->mlx, s->texture[2].image);
 	mlx_destroy_image(s->mlx, s->texture[3].image);
-	//destroy ceiling and floor with if condition ( and door if used)
+	i = -1;
+	while (++i < 9)
+		mlx_destroy_image(s->mlx, s->leaf_dude[i].image);
+	if (s->p->c_is_texture)
+		mlx_destroy_image(s->mlx, s->ceiling_tex.image);
+	if (s->p->f_is_texture)
+		mlx_destroy_image(s->mlx, s->floor_tex.image);
+	
+	//destroy main image
 	mlx_destroy_image(s->mlx, s->mlx_image);
+
+	//destroy window
 	mlx_destroy_window(s->mlx, s->mlx_window);
-	free_double_char(s->p->full_file);
+
 	//free allocations
+	free_double_char(s->p->full_file);
+	free(s->sprite->position);
 	exit(1);
 }
